f96f427b9e7fdc02adf4fc1d8b678ad731c16321
Romano Javier <alexis_b13@live.com.ar>
2021-01-07 09:00:55 -0300

    Cambiar email reautenticacion terminada para poder cambiar email

--- src/components/Settings/UserEmail.js
+++ src/components/Settings/UserEmail.js
@@ -1,19 +1,73 @@
-import React from "react";
-import {Button} from "semantic-ui-react";
+import React, { useState } from "react";
+import { Button, Form, Input, Icon } from "semantic-ui-react";
+import {toast} from "react-toastify";
+import {reauthenticate} from "../../utils/Api"
 
 export default function UserEmail(props) {
   const { user, setShowModal, setTitleModal, setContentModal } = props;
-  
-  const onEdit = () =>{
-      console.log("Ediando email...");
-  }
-  
+
+  const onEdit = () => {
+    setTitleModal("Actualizar email");
+    setContentModal(
+      <ChangeEmailForm email={user.email} setShowModal={setShowModal} />
+    );
+    setShowModal(true);
+  };
+
   return (
     <div className="user-email">
       <h3>Email:{user.email}</h3>
-      <Button circular onClick={onEdit} >
-          Actualizar
+      <Button circular onClick={onEdit}>
+        Actualizar
       </Button>
     </div>
+  );
+}
+
+function ChangeEmailForm(props) {
+  const { email, setShowModal } = props;
+  const [formData, setFormData] = useState({email:"",password:""})
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
+
+  const onSubmit = () => {
+    if (!formData.email) {
+      toast.warning("El email es el mismo");
+    }else{
+      setIsLoading(true);
+      reauthenticate(formData.password).then(()=>{
+        console.log("Auth Correcto.");
+      }).catch(err =>{
+        console.log(err);
+      })
+    }
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Field>
+        <Input 
+        defaultValue={email} 
+        type="text" 
+        onChange={e => setFormData({...formData,email:e.target.value})}
+        />
+      </Form.Field>
+
+      <Form.Field>
+        <Input
+          placeholder="Contraseña"
+          type={showPassword ? "text" : "password"}
+          icon={
+            <Icon
+              name={showPassword ? "eye slash outline" : "eye"}
+              link
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          }
+          onChange={e => setFormData({...formData,password:e.target.value})}
+        />
+      </Form.Field>
+      <Button type="submit" loading={isLoading}>Actualizar Email</Button>
+    </Form>
   );
 }
