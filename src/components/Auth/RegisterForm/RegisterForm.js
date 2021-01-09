abc60315acd968dbc34fe69d5f73d25a32c0a42f
Romano Javier <alexis_b13@live.com.ar>
2021-01-09 15:04:12 -0300

    registrarse y registrarse en cloud firestore

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,73 +1,36 @@
 import React, { useState } from "react";
-import { Button, Icon, Form, Input } from "semantic-ui-react";
+import { Button, Icon, Input, Form } from "semantic-ui-react";
+import { Formik, Field } from "formik";
 import { toast } from "react-toastify";
 import { validateEmail } from "../../../utils/Validations";
+import * as Yup from "yup";
+
 import firebase from "../../../utils/Firebase";
 import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
 import "./RegisterForm.scss";
 
-export default function RegisterForm(props) {
+const db = firebase.firestore(firebase);
+
+const FormUser = (
+  props,
+  { uid, email = "", username = "", password = "", passwordConfirmation = "" }
+) => {
   const { setSelectedForm } = props;
-  const [formData, setFormData] = useState(defaultValueForm());
   const [showPassword, setShowPassword] = useState(false);
   const [formError, setFormError] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-
   const handlerShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
-  const onChange = e => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const onSubmit = () => {
-    setFormError({});
-    let errors = {};
-    let formOk = true;
-
-    if (!validateEmail(formData.email)) {
-      errors.email = true;
-      formOk = false;
-    }
-    if (formData.password.length < 6) {
-      errors.password = true;
-      formOk = false;
-    }
-    if (!formData.username) {
-      errors.username = true;
-      formOk = false;
-    }
-    setFormError(errors);
-
-    if (formOk) {
-      setIsLoading(true);
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(formData.email, formData.password)
-        .then(() => {
-          changeUserName();
-          sendVerificationEmail();
-        })
-        .catch(() => {
-          toast.error("Error al crear la cuenta.");
-        })
-        .finally(() => {
-          setIsLoading(false);
-          setSelectedForm(null);
-        });
-    }
-  };
-
-  const changeUserName = () => {
+  const changeUserName = (values) => {
     firebase
       .auth()
       .currentUser.updateProfile({
-        displayName: formData.username
+        displayName: values.username,
       })
       .catch(() => {
         toast.error("Error al asignar el nombre de usuario.");
@@ -87,79 +50,196 @@ export default function RegisterForm(props) {
   };
 
   return (
-    <div className="register-form">
-      <h1>Registrar usuario</h1>
-      <Form onSubmit={onSubmit} onChange={onChange}>
-        <Form.Field>
-          <Input
-            type="text"
-            name="email"
-            placeholder="Correo electrónico"
-            icon="mail outline"
-            error={formError.email}
-          />
-          {formError.email && (
-            <span className="error-text">
-              Por favor, introduce un correo electronico válido.
-            </span>
-          )}
-        </Form.Field>
-        <Form.Field>
-          <Input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            placeholder="Contraseña"
-            error={formError.password}
-            icon={
-              showPassword ? (
-                <Icon
-                  name="eye slash outline"
-                  link
-                  onClick={handlerShowPassword}
-                />
-              ) : (
-                <Icon name="eye" link onClick={handlerShowPassword} />
-              )
-            }
-          />
-          {formError.password && (
-            <span className="error-text">
-              Por favor, elige una contraseña superior a 5 caracteres.
-            </span>
-          )}
-        </Form.Field>
-        <Form.Field>
-          <Input
-            type="text"
-            name="username"
-            placeholder="¿Como deberíamos llamarte?"
-            icon="user circle outline"
-            error={formError.username}
-          />
-          {formError.username && (
-            <span className="error-text">Por favor, introduce un nombre.</span>
-          )}
-        </Form.Field>
-        <Button type="submit" loading={isLoading}>
-          Continuar
-        </Button>
-      </Form>
+    <Formik
+      initialValues={{
+        email,
+        username,
+        password,
+        passwordConfirmation,
+      }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .min(4, "Debe tener al menos 4 caracteres")
+          .max(50, "Debe tener 50 caracteres o menos")
+          .required("Debes completar este campo"),
+        email: Yup.string()
+          .email("Introduzca un email valido por favor")
+          .required("Debes completar este campo"),
+        // address: Yup.string()
+        //   .min(6, "Debe tener al menos 4 caracteres")
+        //   .max(50, "Debe tener 50 caracteres o menos")
+        //   .required("Debes completar este campo"),
+        // phoneNumber: Yup.string()
+        //   .required("Please Enter your Phone Number")
+        //   .matches(
+        //     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+        //     "Phone number is not valid"
+        //   ),
+        password: Yup.string()
+          .required("Please Enter your password")
+          .matches(
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+          ),
+        passwordConfirmation: Yup.string()
+          .oneOf([Yup.ref("password"), null], "La contraseña no coincide")
+          .required("Password confirm is required"),
+      })}
+      onSubmit={async (values, { setSubmitting, resetForm }) => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(values.email, values.password)
+          .then((response) => {
+            changeUserName(values);
+            sendVerificationEmail();
+            const data = {
+              uid: response.user.uid,
+              username:values.username,
+              email: values.email,
+              pacientes:[]
+            };
 
-      <div className="register-form__options">
-        <p onClick={() => setSelectedForm(null)}>Volver</p>
-        {/* <p>
+            // Add a new document in collection "cities" with ID 'LA'
+            setTimeout(() => {
+              db.collection("users")
+                .doc(response.user.uid)
+                .set(data)
+                .then(() => console.log("ok"));
+            }, 3000);
+          })
+          .catch(() => {
+            toast.error("Error al crear la cuenta.");
+          })
+          .finally(() => {
+            setIsLoading(false);
+            // const data = {
+            //   name: 'Los Angeles',
+            //   state: 'CA',
+            //   country: 'USA'
+            // };
+
+            // // Add a new document in collection "cities" with ID 'LA'
+            // db.collection('cities').doc('LA').set(data).then(resd=>console.log(resd));
+            setSelectedForm(null);
+          });
+      }}
+    >
+      {({
+        isValid,
+        isSubmitting,
+        setFieldValue,
+        errors,
+        touched,
+        handleSubmit,
+        handleReset,
+        handleChange,
+      }) => {
+        return (
+          <div className="register-form">
+            <h1>Registrar usuario</h1>
+            <Form onSubmit={handleSubmit} onChange={handleChange}>
+              <Field name="email">
+                {({ field, form, meta }) => (
+                  <div>
+                    <Input
+                      type="text"
+                      {...field}
+                      placeholder="Correo electrónico"
+                      icon="mail outline"
+                    />
+                    {errors.email && touched.email ? (
+                      <div>{errors.email}</div>
+                    ) : null}
+                  </div>
+                )}
+              </Field>
+
+              <Field name="password">
+                {({ field, form, meta }) => (
+                  <div>
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      {...field}
+                      placeholder="Contraseña"
+                      icon={
+                        showPassword ? (
+                          <Icon
+                            name="eye slash outline"
+                            link
+                            onClick={handlerShowPassword}
+                          />
+                        ) : (
+                          <Icon name="eye" link onClick={handlerShowPassword} />
+                        )
+                      }
+                    />
+                    {errors.password && touched.password ? (
+                      <div>{errors.password}</div>
+                    ) : null}
+                  </div>
+                )}
+              </Field>
+
+              <Field name="passwordConfirmation">
+                {({ field, form, meta }) => (
+                  <div>
+                    <Input
+                      type={showPassword ? "text" : "password"}
+                      {...field}
+                      placeholder="Contraseña"
+                      icon={
+                        showPassword ? (
+                          <Icon
+                            name="eye slash outline"
+                            link
+                            onClick={handlerShowPassword}
+                          />
+                        ) : (
+                          <Icon name="eye" link onClick={handlerShowPassword} />
+                        )
+                      }
+                    />
+                    {errors.passwordConfirmation &&
+                    touched.passwordConfirmation ? (
+                      <div>{errors.passwordConfirmation}</div>
+                    ) : null}
+                  </div>
+                )}
+              </Field>
+
+              <Field name="username">
+                {({ field, form, meta }) => (
+                  <div>
+                    <Input
+                      type="text"
+                      {...field}
+                      placeholder="¿Como deberíamos llamarte?"
+                      icon="user circle outline"
+                    />
+                    {errors.username && touched.username ? (
+                      <div>{errors.username}</div>
+                    ) : null}
+                  </div>
+                )}
+              </Field>
+
+              <Button type="submit" loading={isLoading}>
+                Continuar
+              </Button>
+            </Form>
+
+            <div className="register-form__options">
+              <p onClick={() => setSelectedForm(null)}>Volver</p>
+              {/* <p>
           ¿Ya tienes Musicfy?{" "}
           <span onClick={() => setSelectedForm("login")}>Iniciar sesión</span>
         </p> */}
-      </div>
-    </div>
+            </div>
+          </div>
+        );
+      }}
+    </Formik>
   );
-}
+};
 
-function defaultValueForm() {
-  return {
-    email: "",
-    password: "",
-    username: ""
-  };
-}
+export default FormUser;
